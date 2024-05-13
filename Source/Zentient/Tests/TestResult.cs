cd977b212271b018a9b108c0e089bf6ad7f0a78f
ulfbo <ulfbourelius71@gmail.com>
2024-05-13 18:15:57 +0200

    Added TestLogEntry and TestResult

---
+++ Source/Zentient/Tests/TestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zentient.Tests;
+
+public enum TestResultType
+{
+    Fail = 1, Success = 2, Stopped = 3
+}
+
+public class TestResult(TestResultType type, string message)
+{
+    public TestResultType resultType { get; set; } = type;
+    public string message { get; set; } = message;
+}
