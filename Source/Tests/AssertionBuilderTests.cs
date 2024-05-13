0eb5ec147a94c7b5cc1d8b4e414eb507d0636d4e
ulfbo <ulfbourelius71@gmail.com>
2024-05-13 12:58:36 +0200

    Initial commit for Tests project

---
+++ Source/Tests/AssertionBuilderTests.cs
@@ -0,0 +1,141 @@
+﻿using Zentient.Tests;
+
+
+namespace Tests;
+
+[TestClass]
+public class AssertionBuilderTests
+{
+    [TestMethod]
+    public void AssertThatString_IsEqualTo_ShouldPass_WhenObjectsAreEqual()
+    {
+        // Arrange
+        var subject = new String("Same");
+        var expected = new String("Same");
+
+        // Act & Assert
+        Validate.Success(() => Zentient.Tests.Assert
+            .That<string>(subject)
+            .IsEqualTo(expected));
+    }
+
+    [TestMethod]
+    public void AssertThatString_IsEqualTo_ShouldFail_WhenObjectsAreNotEqual()
+    {
+        // Arrange
+        var subject = new String("Same");
+        var expected = new String("Different");
+
+        // Act & Assert
+        Validate.Fail(() => Zentient.Tests.Assert
+            .That<string>(subject)
+            .IsEqualTo(expected));
+    }
+
+    [TestMethod]
+    public void AssertThatMyClass_IsNotEqualTo_ShouldPass_WhenObjectsAreNotEqual()
+    {
+        // Arrange
+        var subject = new MyClass();
+        var different = new MyClass();
+
+        // Act & Assert
+        Validate.Success(() => Zentient.Tests.Assert
+            .That(subject)
+            .IsNotEqualTo(different));
+    }
+
+    [TestMethod]
+    public void AssertThatMyClass_IsNotEqualTo_ShouldFail_WhenObjectsAreEqual()
+    {
+        // Arrange
+        var subject = new MyClass();
+        var same = subject;
+
+        // Act & Assert
+        Validate.Fail(() => Zentient.Tests.Assert
+            .That(subject)
+            .IsNotEqualTo(same));
+    }
+
+    [TestMethod]
+    public void AssertThatObject_IsSameAs_ShouldPass_WhenObjectsAreSameInstance()
+    {
+        // Arrange
+        var subject = new object();
+        var expected = subject;
+
+        // Act & Assert
+        Validate.Success(() => Zentient.Tests.Assert
+            .That<object>(subject)
+            .IsSameAs(expected));
+    }
+
+    [TestMethod]
+    public void AssertThatObject_IsSameAs_ShouldFail_WhenObjectsAreNotSameInstance()
+    {
+        // Arrange
+        var subject = new object();
+        var expected = new object();
+
+        // Act & Assert
+        Validate.Fail(() => Zentient.Tests.Assert
+            .That<object>(subject)
+            .IsSameAs(expected));
+    }
+
+    [TestMethod]
+    public void AssertThatObject_IsNull_ShouldPass_WhenObjectIsNull()
+    {
+        // Arrange
+        object subject = null;
+
+        // Act & Assert
+        Validate.Success(() => Zentient.Tests.Assert
+            .That<object>(subject)
+            .IsNull());
+    }
+
+    [TestMethod]
+    public void AssertThatObject_IsNull_ShouldFail_WhenObjectIsNotNull()
+    {
+        // Arrange
+        var subject = new object();
+
+        // Act & Assert
+        Validate.Fail(() => Zentient.Tests.Assert
+            .That<object>(subject)
+            .IsNull());
+    }
+
+    [TestMethod]
+    public void AssertThatObject_IsNotNull_ShouldPass_WhenObjectIsNotNull()
+    {
+        // Arrange
+        var subject = new object();
+
+        // Act & Assert
+        Validate.Success(() => Zentient.Tests.Assert
+            .That<object>(subject)
+            .IsNotNull());
+    }
+
+    [TestMethod]
+    public void AssertThatObject_IsNotNull_ShouldFail_WhenObjectIsNull()
+    {
+        // Arrange
+        object subject = null;
+
+        // Act & Assert
+        Validate.Fail(() => Zentient.Tests.Assert
+            .That<object>(subject)
+            .IsNotNull());
+    }
+}
+
+internal class MyClass
+{
+    public MyClass()
+    {
+    }
+}
---
+++ Source/Tests/ExceptionAssertionBuilderTests.cs
@@ -0,0 +1,327 @@
+//
+// File: ExceptionAssertionBuilderTests.cs
+// 
+// Description:
+// Defines a set of fluent assertion methods for evaluating equality, type compatibility, and other logical conditions. These methods empower developers to confirm the expected behavior and results of the code under test. The IAssertBuilder interface promotes a more intuitive and sustainable testing practice by supporting a chainable method syntax.
+// 
+// Usage:
+// The IAssertBuilder interface is implemented to facilitate unit testing assertions on object states. Upon instantiation with the target test object, it offers a sequence of chainable methods designed to throw exceptions upon assertion failure, thereby indicating test failures.
+// 
+// Purpose:
+// The IAssertBuilder interface aims to provide a straightforward and articulate means for crafting test assertions. Leveraging a fluent interface pattern, it simplifies test code structure and enhances the clarity of test assertions, aiding developers in grasping the test's purpose and the standards for its success.
+// 
+// MIT License
+// 
+// Copyright (c) 2024 Ulf Bourelius
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using Zentient.Tests;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Tests;
+
+
+[TestClass]
+public class ExceptionAssertionBuilderTests
+{
+    public static readonly string BaseMessage = "Base exception";
+    public static readonly string DerivedMessage = "Derived exception";
+    public static readonly string OtherDerivedMessage = "Other Derived exception";
+    public static readonly string NotDerivedMessage = "Not Derived exception";
+
+    public static Action ThrowsBase = () => throw new Exceptions(BaseMessage);
+    public static Action ThrowsDerived = () => throw new DerivedException(DerivedMessage);
+    public static Action ThrowsOtherDerived = () => throw new OtherDerivedException(OtherDerivedMessage);
+    public static Action ThrowsNotDerived => () => throw new NotDerivedException(NotDerivedMessage);
+    public static Action DoesNotThrowAny = () => {};
+
+    // Validate Throws<T>
+    [TestMethod]
+    public void AssertThatThrowsBase_IsValidatedBy_ThrowsBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .Throws<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsDerived_IsValidatedBy_ThrowsBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsDerived)
+        .Throws<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsOtherDerived_IsValidatedBy_ThrowsBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsOtherDerived)
+        .Throws<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsNotDerived_IsInvalidatedBy_ThrowsBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsNotDerived)
+        .Throws<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatDoesNotThrowAny_IsInvalidatedBy_ThrowsBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(DoesNotThrowAny)
+        .Throws<Exceptions>());
+    }
+
+    // Validate ThrowsExactly<T>
+    [TestMethod]
+    public void AssertThatThrowsBase_IsValidatedBy_ThrowsExactlyBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .ThrowsExactly<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsDerived_IsInvalidatedBy_ThrowsExactlyBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsDerived)
+        .ThrowsExactly<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsBase_IsInvalidatedBy_ThrowsExactlyDerivedException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .ThrowsExactly<DerivedException>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsNotDerived_IsInvalidatedBy_ThrowsExactlyBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsNotDerived)
+        .ThrowsExactly<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsBase_IsInvalidatedBy_ThrowsDerivedBaseException()
+    {
+        // Validates ThrowsDerived<T>
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .ThrowsDerived<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsDerived_IsValidatedBy_ThrowsDerivedBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsDerived)
+        .ThrowsDerived<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsOtherDerived_IsValidatedBy_ThrowsDerivedBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsOtherDerived)
+        .ThrowsDerived<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsBase_IsInvalidatedBy_ThrowsDerivedDerivedException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .ThrowsDerived<DerivedException>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsNotDerived_IsInvalidatedBy_ThrowsDerivedBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsNotDerived)
+        .ThrowsDerived<Exceptions>());
+    }
+
+    // ThrowsAny
+    [TestMethod]
+    public void AssertThatThrowsBase_IsValidatedBy_ThrowsAny()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .ThrowsAny());
+    }
+
+    [TestMethod]
+    public void AssertThatDoesNotThrowAny_IsInvalidatedBy_ThrowsAny()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(DoesNotThrowAny)
+        .ThrowsAny());
+    }
+
+    // Validates DoesNotThrow<T>
+    [TestMethod]
+    public void AssertThatThrowsBase_IsInvalidatedBy_DoesNotThrowBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .DoesNotThrow<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsDerived_IsInvalidatedBy_DoesNotThrowBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsDerived)
+        .DoesNotThrow<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsOtherDerived_IsInvalidatedBy_DoesNotThrowBaseException()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsOtherDerived)
+        .DoesNotThrow<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsNotDerived_IsValidatedBy_DoesNotThrowBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsNotDerived)
+        .DoesNotThrow<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatDoesNotThrowAny_IsValidatedBy_DoesNotThrowBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(DoesNotThrowAny)
+        .DoesNotThrow<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsBase_IsInvalidatedBy_DoesNotThrowExactlyBaseException()
+    {
+        // Validate DoesNotThrowExactly<T>
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .DoesNotThrowExactly<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsDerived_IsValidatedBy_DoesNotThrowExactlyBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsDerived)
+        .DoesNotThrowExactly<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsBase_IsValidatedBy_DoesNotThrowExactlyDerivedException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .DoesNotThrowExactly<DerivedException>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsNotDerived_IsValidatedBy_DoesNotThrowExactlyBaseException()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsNotDerived)
+        .DoesNotThrowExactly<Exceptions>());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsBase_IsInvalidatedBy_DoesNotThrow()
+    {
+        // ThrowsAny
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .DoesNotThrow());
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsBase_IsInvalidatedBy_DoesNotThrowAny()
+    {
+        Validate.Fail(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .DoesNotThrowAny());
+    }
+
+    [TestMethod]
+    public void AssertThatDoesNotThrowAny_IsValidatedBy_DoesNotThrow()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(DoesNotThrowAny)
+        .DoesNotThrow());
+    }
+
+    [TestMethod]
+    public void AssertThatDoesNotThrowAny_IsValidatedBy_DoesNotThrowAny()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(DoesNotThrowAny)
+        .DoesNotThrowAny());
+    }
+
+    // Validate WithMessage
+    [TestMethod]
+    public void AssertThatThrowsBase_IsValidatedBy_ThrowsBaseException_WithMessageBaseMessage()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsBase)
+        .Throws<Exceptions>()
+        .WithMessage(BaseMessage));
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsDerived_IsValidatedBy_ThrowsBaseException_WithMessageContaining_Substring()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsDerived)
+        .Throws<Exceptions>()
+        .WithMessageContaining(DerivedMessage.Substring(0, 1)));
+    }
+
+    [TestMethod]
+    public void AssertThatThrowsOtherDerived_IsValidatedBy_ThrowsBaseException_WithMessageOtherDerivedMessage()
+    {
+        Validate.Success(() => Zentient.Tests.Assert
+        .That(ThrowsOtherDerived)
+        .Throws<Exceptions>()
+        .WithMessage(OtherDerivedMessage));
+    }
+}
